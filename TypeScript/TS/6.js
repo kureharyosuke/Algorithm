634caefb88da9501f746c2aa59b934970634beaa
kureharyosuke <ohhayo.com@gmail.com>
2021-04-20 00:04:40 +0900

    test: false

---
+++ TypeScript/TS/6.js
@@ -0,0 +1,16 @@
+"use strict";
+// 문제6: False;
+// 다음은 자바스크립트 문법 중에서 False로 취급하는 것들 입니다.
+// 앗, False로 취급하지 않는 것이 하나 있네요! **True를 찾아주세요.**
+// 1)  NaN
+// 2)  1
+// 3)  ""
+// 4)  0
+// 5)  undefined
+// 2;
+console.log(!NaN);
+console.log(!1);
+console.log(!"");
+console.log(!0);
+console.log(!undefined);
+console.log(isNaN(1));
