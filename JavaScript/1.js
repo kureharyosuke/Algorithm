a31cc71d3dc7cfeebbd14b509159330bcaaa2750
kureharyosuke <ohhayo.com@gmail.com>
2021-04-19 23:14:55 +0900

    Array.splice()

--- JavaScript/1.js
+++ JavaScript/1.js
@@ -3,9 +3,13 @@
 
 var nums = [100, 200, 300, 400, 500];
 
-nums.pop();
-nums.pop();
+// nums.pop();
+// nums.pop();
+// console.log(nums);
+// var nums1 = nums.slice(0, 3);
+// // numslice = nums.slice(3, 2);
+// console.log(nums1);
+
+let numSplice = nums.splice(3, 2);
+
 console.log(nums);
-var nums1 = nums.slice(0, 3);
-// numslice = nums.slice(3, 2);
-console.log(nums1);
