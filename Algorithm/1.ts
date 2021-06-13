ba84f7d694b12209d9b4c7e458ad4c8ca6c8a7ad
kureharyosuke <ohhayo.com@gmail.com>
2021-06-13 20:54:44 +0900

    1. 세 수중에 최소값

--- Algorithm/1.ts
+++ Algorithm/1.ts
@@ -1,3 +1,18 @@
 /**
+ *  세 수 중 최소 값
  *
+ * 100이하의 자연수 A, B, C를 입력받아, 세 수 중 가장 작은 값을 출력하는 프로그램을 작성하세요.
+ * (*정렬을 사용하면 안됩니다.) if문으로만,
  */
+
+function solution(a: number, b: number, c: number) {
+  let answer: number;
+
+  if (a < b) answer = a;
+  else answer = b;
+  if (c < answer) answer = c;
+
+  return answer;
+}
+
+console.log(solution(6, 4, 11));
