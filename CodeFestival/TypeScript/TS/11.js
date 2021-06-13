70d4f52d57afaaba2b8713e19c1b6eeaf8ad1921
kureharyosuke <ohhayo.com@gmail.com>
2021-06-13 21:05:48 +0900

    11

---
+++ CodeFestival/TypeScript/TS/11.js
@@ -0,0 +1,11 @@
+"use strict";
+/**
+ *
+ *
+ */
+let s = 0;
+for (let i = 1; i <= 100; i++) {
+  s = s + i;
+  console.log(s);
+}
+console.log(s);
---
+++ CodeFestival/TypeScript/TS/11.ts
@@ -0,0 +1,12 @@
+/**
+ *
+ *
+ */
+
+let s: number = 0;
+
+for (let i = 1; i <= 100; i++) {
+  s = s + i;
+}
+
+console.log(s);
