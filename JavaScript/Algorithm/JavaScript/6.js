d01360e846ef0b37acde58f5477ba532ab398b13
kureharyosuke <ohhayo.com@gmail.com>
2022-05-19 23:48:05 +0900

    6 홀수의 최소값과 홀수의 합 + 3

--- JavaScript/Algorithm/JavaScript/6.js
+++ JavaScript/Algorithm/JavaScript/6.js
@@ -14,8 +14,8 @@ console.time("solutionJS6_1");
 function solutionJS6_1(number) {
   const filterd = number.filter((num) => num % 2 !== 0);
   const filterMinNum = Math.min(...filterd);
-  const test = filterd.reduce((acc, cur) => acc + cur);
-  return `min number :${filterMinNum}, reduce : ${test}`;
+  const filterdReduce = filterd.reduce((acc, cur) => acc + cur);
+  return `min number :${filterMinNum}, reduce : ${filterdReduce}`;
 }
 console.timeEnd("solutionJS6_1"); //solutionJS6_1: 0.055ms
 
@@ -23,8 +23,8 @@ console.time("solutionJS6_2");
 const solutionJS6_2 = (number) => {
   const filterd = number.filter((num) => num % 2 !== 0);
   const filterMinNum = Math.min(...filterd);
-  const test = filterd.reduce((acc, cur) => acc + cur);
-  return `min number :${filterMinNum}, reduce : ${test}`;
+  const filterdReduce = filterd.reduce((acc, cur) => acc + cur);
+  return `min number :${filterMinNum}, reduce : ${filterdReduce}`;
 };
 console.timeEnd("solutionJS6_2"); //solutionJS6_2: 0.003ms
 
@@ -36,6 +36,55 @@ const solutionJS6_3 = (number) => {
 };
 console.timeEnd("solutionJS6_3"); //solutionJS6_3: 0.002ms
 
+console.time("solutionJS6_4");
+function solutionJS6_4(number) {
+  const minNumber = Math.min(...number.filter((num) => num % 2 !== 0));
+  const reduce = number.filter((num) => num % 2 !== 0).reduce((acc, cur) => acc + cur);
+  return `min number :${minNumber}, reduce : ${reduce}`;
+}
+console.timeEnd("solutionJS6_4"); //solutionJS6_4: 0.017ms
+
+console.time("solutionJS6_5");
+const solutionJS6_5 = (number) => {
+  const answer = [];
+  const filterd = number.filter((num) => num % 2 !== 0);
+  const filterMinNum = Math.min(...filterd);
+  const filterdReduce = filterd.reduce((acc, cur) => acc + cur);
+  answer.push(filterMinNum, filterdReduce);
+  return answer;
+};
+console.timeEnd("solutionJS6_5");
+
+console.time("solutionJS6_6");
+const solutionJS6_6 = (number) => {
+  const answer = [];
+  const minNumber = Math.min(...number.filter((num) => num % 2 !== 0));
+  const reduce = number.filter((num) => num % 2 !== 0).reduce((acc, cur) => acc + cur);
+  answer.push(minNumber, reduce);
+  return answer;
+};
+console.timeEnd("solutionJS6_6"); // solutionJS6_6: 0.001ms
+
+console.time("solutionJS6_7");
+function solutionJS6_7(number) {
+  const answer = [];
+  let sum = 0;
+  let min = Number.MAX_SAFE_INTEGER;
+  for (let n of number) {
+    if (n % 2 !== 0) {
+      sum += n;
+      if (n < min) min = n;
+    }
+  }
+  answer.push(min, sum);
+  return answer;
+}
+console.timeEnd("solutionJS6_7");
+
 console.log(solutionJS6_1([12, 77, 38, 41, 53, 92, 85]));
 console.log(solutionJS6_2([12, 77, 38, 41, 53, 92, 85]));
 console.log(solutionJS6_3([12, 77, 38, 41, 53, 92, 85]));
+console.log(solutionJS6_4([12, 77, 38, 41, 53, 92, 85]));
+console.log(solutionJS6_5([12, 77, 38, 41, 53, 92, 85]));
+console.log(solutionJS6_6([12, 77, 38, 41, 53, 92, 85]));
+console.log(solutionJS6_7([12, 77, 38, 41, 53, 92, 85]));
