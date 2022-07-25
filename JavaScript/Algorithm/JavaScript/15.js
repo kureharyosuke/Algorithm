2086a90dc49fd941448f1cc3987a8c8b551ec429
kureharyosuke <ohhayo.com@gmail.com>
2022-07-26 00:13:07 +0900

    solutionJS15

---
+++ JavaScript/Algorithm/JavaScript/15.js
@@ -0,0 +1,50 @@
+/**
+ * 가장 긴 문자열
+ *
+ * 입력 예제
+ * study
+ *
+ * 출력 예제
+ * u
+ *
+ * 입력 예제
+ * good
+ *
+ * 출력 예제
+ * oo
+ *
+ */
+
+console.time("solutionJS15_0");
+// const start = performance.now();
+
+function solutionJS15_0(str) {
+  let answer;
+  let mid = Math.floor(str.length / 2);
+
+  if (str.length % 2 === 1) answer = str.substring(mid, mid + 1); // index 번호
+  else answer = answer = str.substring(mid - 1, mid + 1);
+  return answer;
+}
+
+// const end = performance.now();
+// console.log(`${end - start}ms`);
+console.timeEnd("solutionJS15_0");
+
+console.log(solutionJS15_0("study"), "solutionJS15_0");
+console.log(solutionJS15_0("good"), "solutionJS15_0");
+
+// const start1 = performance.now();
+console.time("solutionJS15_1");
+
+function solutionJS15_1(str) {
+  const mid = Math.floor(str.length / 2);
+  return str.length % 2 === 1
+    ? str.substring(mid, mid + 1)
+    : str.substring(mid - 1, mid + 1);
+}
+
+console.timeEnd("solutionJS15_1");
+
+console.log(solutionJS15_1("study"), "solutionJS15_1");
+console.log(solutionJS15_1("good"), "solutionJS15_1");
