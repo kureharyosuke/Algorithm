85acc9d3767e4656577cae8046132413c556e83e
kureharyosuke <ohhayo.com@gmail.com>
2022-08-02 23:22:29 +0900

    solutionJS20_1

--- JavaScript/Algorithm/JavaScript/20.js
+++ JavaScript/Algorithm/JavaScript/20.js
@@ -26,7 +26,7 @@
  * 3  보
  */
 
-function solutionJS20(a, b) {
+function solutionJS20_0(a, b) {
   let answer = "";
   for (let i = 0; i < a.length; i++) {
     if (a[i] === b[i]) answer += "D" + " ";
@@ -35,7 +35,6 @@ function solutionJS20(a, b) {
     else if (a[i] === 3 && b[i] === 2) answer += "A" + " ";
     else answer += "B" + " ";
   }
-
   return answer;
 }
 
@@ -43,4 +42,17 @@ function solutionJS20(a, b) {
 //       A, B, A, B, D
 let a = [2, 3, 3, 1, 3];
 let b = [1, 1, 2, 2, 3];
-console.log(solutionJS20(a, b));
+console.log(solutionJS20_0(a, b));
+
+function solutionJS20_1(a, b) {
+  let answer = "";
+  for (const [index, value] of a.entries()) {
+    if (a[index] === b[index]) answer += "D" + " ";
+    else if (a[index] === 1 && b[index] === 3) answer += "A" + " ";
+    else if (a[index] === 2 && b[index] === 1) answer += "A" + " ";
+    else if (a[index] === 3 && b[index] === 2) answer += "A" + " ";
+    else answer += "B" + " ";
+  }
+  return answer;
+}
+console.log(solutionJS20_1(a, b));
