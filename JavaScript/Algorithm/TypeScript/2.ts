4ad219d89b9e521d2eb152f58eac3e0d9c97f38d
kureharyosuke <ohhayo.com@gmail.com>
2023-03-21 22:23:35 +0900

    fix: rest of min

--- JavaScript/Algorithm/TypeScript/2.ts
+++ JavaScript/Algorithm/TypeScript/2.ts
@@ -2,20 +2,24 @@
  *
  */
 
-function solution2(a: number, b: number, c: number): string {
-  let answer: string = "YES";
-  let max: number;
-  let total: number = a + b + c;
+function solutionTS2(a: number, b: number, c: number): string {
+	let answer: string = "YES";
+	let max: number;
+	let total: number = a + b + c;
 
-  if (a > b) max = a;
-  // 1. 13 > 33 false
-  else max = b;
-  // 2. max = 33
-  if (c > max) max = c;
-  // 3. 17 > 33 = false
-  if (total - max <= max) answer = "No";
+	if (a > b) max = a;
+	// 1. 13 > 33 false
+	else max = b;
+	// 2. max = 33
+	if (c > max) max = c;
+	// 3. 17 > 33 = false
+	if (total - max <= max) answer = "No";
 
-  return answer;
+	return answer;
 }
 
-console.log(solution2(13, 33, 17)); // OUTPUT: NO
+console.log(solutionTS2(13, 33, 17)); // OUTPUT: NO
+
+function solutionTS2_1(...numbers: number[]): number {
+	return Math.min(...numbers);
+}
