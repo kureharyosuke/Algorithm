695e42005e05b6c4fbc1a01dc410407603cc06de
kureharyosuke <ohhayo.com@gmail.com>
2023-03-22 13:39:16 +0900

    fix: find Min

---
+++ Golang/1.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+  a, b, c := 5, 3, 7
+  fmt.Println(findMin(a, b, c))
+}
+
+func findMin(a, b, c int) int {
+  answer := a
+
+  if b < answer {
+    answer = b
+  }
+
+  if c < answer {
+    answer = c
+  }
+
+  return answer
+}
