49ca87a507a3111b32e08de972b42e167c4bd52b
kureharyosuke <ohhayo.com@gmail.com>
2023-05-22 01:56:37 +0900

    fix: 25

--- JavaScript/Algorithm/JavaScript/25.js
+++ JavaScript/Algorithm/JavaScript/25.js
@@ -13,59 +13,62 @@
 // * ▣ 입력예제 1 gooG
 // * ▣ 출력예제 1 YES
 
-const str = 'gooG'
+const str = "gooG";
 
-console.time('solutionJS25_0')
-function solutionJS25_0 (){}
-console.timeEnd('solutionJS25_0')
+console.time("solutionJS25_0");
+function solutionJS25_0() {}
+console.timeEnd("solutionJS25_0");
 
-console.time('solutionJS25_1')
-function solutionJS25_1 (string){
-    const str = string.toLowerCase()
-    return str.split().reverse().join() === str ? 'YES' : 'NO'
+console.time("solutionJS25_1");
+function solutionJS25_1(string) {
+  const str = string.toLowerCase();
+  return str.split("").reverse().join() === str ? "YES" : "NO";
 }
 console.log(solutionJS25_1(str));
-console.timeEnd('solutionJS25_1')
+console.timeEnd("solutionJS25_1");
 
-console.time('solutionJS25_2')
-const solutionJS25_2 = (string) => string.toLowerCase().split().reverse().join() === string.toLowerCase() ? 'YES' : 'NO'
+console.time("solutionJS25_2");
+const solutionJS25_2 = (string) =>
+  string.toLowerCase().split().reverse().join() === string.toLowerCase()
+    ? "YES"
+    : "NO";
 console.log(solutionJS25_2(str));
-console.timeEnd('solutionJS25_2')
+console.timeEnd("solutionJS25_2");
 
-console.time('solutionJS25_3')
-function solutionJS25_3 (string){
-   // 문자열을 모두 소문자로 바꿉니다.
-   const str = string.toLowerCase();
+console.time("solutionJS25_3");
+function solutionJS25_3(string) {
+  // 문자열을 모두 소문자로 바꿉니다.
+  const str = string.toLowerCase();
 
-   // 문자열의 양 끝에서부터 한 글자씩 비교합니다.
-   for (let i = 0, j = str.length - 1; i < j; i++, j--) {
-     // 문자가 다르다면 회문 문자열이 아닙니다.
-     if (str[i] !== str[j]) {
-       return 'NO';
-     }
-   }
- 
-   // 모든 문자가 같다면 회문 문자열입니다.
-   return 'YES';
+  // 문자열의 양 끝에서부터 한 글자씩 비교합니다.
+  for (let i = 0, j = str.length - 1; i < j; i++, j--) {
+    // 문자가 다르다면 회문 문자열이 아닙니다.
+    if (str[i] !== str[j]) {
+      return "NO";
+    }
+  }
+
+  // 모든 문자가 같다면 회문 문자열입니다.
+  return "YES";
 }
-console.log(solutionJS25_3(str))
-console.timeEnd('solutionJS25_3')
+console.log(solutionJS25_3(str));
+console.timeEnd("solutionJS25_3");
 
-console.time('solutionJS25_4')
-function solutionJS25_4 (string){
-    // 문자열을 모두 소문자로 바꿉니다.
+console.time("solutionJS25_4");
+function solutionJS25_4(string) {
+  // 문자열을 모두 소문자로 바꿉니다.
   const str = string.toLowerCase();
 
   // 문자열의 반만 검사합니다.
   for (let i = 0, len = Math.floor(str.length / 2); i < len; i++) {
     // 문자가 다르다면 회문 문자열이 아닙니다.
     if (str[i] !== str[str.length - 1 - i]) {
-      return 'NO';
+      return "NO";
     }
   }
 
   // 모든 문자가 같다면 회문 문자열입니다.
-  return 'YES';
+  return "YES";
 }
-console.log(solutionJS25_4(str))
-console.timeEnd('solutionJS25_4')
+console.log(solutionJS25_4(str));
+console.timeEnd("solutionJS25_4");
