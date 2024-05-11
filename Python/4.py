9af5ad4b3f36b0c40603a342cd7beb8faaa9c18e
KurehaRyosuke <ohhayo.com@gmail.com>
2024-05-12 02:54:43 +0900

    test: 4

--- Python/4.py
+++ Python/4.py
@@ -1,7 +1,9 @@
-def solution4(array, divisor):
+def solution4_1(array, divisor):
   result = [num for num in array if num % divisor == 0]
   return result if result else [-1]
   
-print(solution4([5, 7, 6, 10], 2)) 
-print(solution4([5, 9, 7, 10], 5))  # [5, 10]
-print(solution4([3, 7, 11], 5))     # [-1]
+print(solution4_1([5, 7, 6, 10], 2)) 
+print(solution4_1([5, 9, 7, 10], 5))  
+print(solution4_1([3, 7, 11], 5))     
+
+# def solution4_2(array, divisor):
